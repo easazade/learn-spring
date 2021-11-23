b433e37a1f1cb06e608c72f3dfead6c0281a4a94
alireza easazade <easazade@gmail.com>
2021-11-23 21:04:27 +0330

    udpate readme.md

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ This is all the small practice projects I did to learn spring boot framework.
 
 
 **NOTE:** all the projects are just written for the purpose of learning and playing around so I don't pay alot of attention what git commit messages i use or whether my code is formatted correctly or not.
+
+### Architecture
+<img src="architecture.png">
--- level1/src/test/kotlin/com/example/demo/BankServiceTest.kt
+++ level1/src/test/kotlin/com/example/demo/BankServiceTest.kt
@@ -1,20 +1,21 @@
 package com.example.demo
 
 import com.example.demo.data.datasource.BankDataSource
-import com.example.demo.data.datasource.MockBankDataSource
 import com.example.demo.data.models.Bank
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import io.mockk.*
 
 internal class BankServiceTest {
 
-  private val fakeBanks = listOf(Bank("01",1.0,1))
-  private val datasource:BankDataSource = mockk(relaxed = true)
+  private val fakeBanks = listOf(Bank("01", 1.0, 1))
+  private val datasource: BankDataSource = mockk(relaxed = true)
   private val service = BankService(datasource)
 
   @Test
-  fun `should call its datasource to retrieve banks`(){
+  fun `should call its datasource to retrieve banks`() {
     // with
     every { datasource.getBanks() } returns fakeBanks
     // when
