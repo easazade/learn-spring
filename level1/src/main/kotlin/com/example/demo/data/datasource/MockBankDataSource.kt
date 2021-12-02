b7d47d6a487e468743faadf7a367a0c9d96b378d
alireza easazade <easazade@gmail.com>
2021-12-02 06:54:08 +0330

    add more notes && lazy initialization to level1 project

--- level1/src/main/kotlin/com/example/demo/data/datasource/MockBankDataSource.kt
+++ level1/src/main/kotlin/com/example/demo/data/datasource/MockBankDataSource.kt
@@ -1,8 +1,10 @@
 package com.example.demo.data.datasource
 
 import com.example.demo.data.models.Bank
+import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Repository
 
+@Primary
 @Repository("mock")
 class MockBankDataSource : BankDataSource {
 
