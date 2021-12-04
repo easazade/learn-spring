115b80d1e7fe16e1b6d778dfe659d5a9f3883c8f
alireza easazade <easazade@gmail.com>
2021-12-04 09:19:58 +0330

    add ApplicationRunner example

--- level1/src/main/kotlin/com/example/demo/utils/AppArgs.kt
+++ level1/src/main/kotlin/com/example/demo/utils/AppArgs.kt
@@ -1,12 +1,17 @@
 package com.example.demo.utils
 
 import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 
+// 2 ways that we setup
+
+
 @Component
-class AppArgs(args: ApplicationArguments) {
+class AppArgs(args: ApplicationArguments){
 
   init {
+    // here we could setup things after spring boot app has been setup and before it starts accepting traffic
     println("###################### INITING AppArgs")
     val nonOptionArgs= args.nonOptionArgs
     nonOptionArgs.forEach {
@@ -17,3 +22,21 @@ class AppArgs(args: ApplicationArguments) {
     }
   }
 }
+
+@Component
+class AppArgs2 : ApplicationRunner{
+
+
+  override fun run(args: ApplicationArguments?) {
+    // here we could setup things after spring boot app has been setup and before it starts accepting traffic
+    println("###################### INITING AppArgs2")
+    val nonOptionArgs= args?.nonOptionArgs
+    nonOptionArgs?.forEach {
+      println(it)
+    }
+    args?.optionNames?.forEach {
+      println("###### ARGS => $it = ${args?.getOptionValues(it)}")
+    }
+  }
+}
+
